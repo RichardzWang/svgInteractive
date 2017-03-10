89ea8eb462f38117dfef66b4edad0ab4894543c0
Richard Wang <rwang4@stuy.edu>
2017-03-10 00:41:04 -0500

    done

--- svg-interact.js
+++ svg-interact.js
@@ -1,6 +1,8 @@
 var svg = document.getElementById("vector");
 var rid = 0;
 
+var move = false;
+
 var addRandom = function(e) {
     r = 20;
     x = Math.random()*500;
@@ -26,11 +28,13 @@ var makeDot = function(x, y) {
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c.setAttribute("cx", x);
     c.setAttribute("cy", y);
+    c.setAttribute("vx", 1);
+    c.setAttribute("vy", 1);
     c.setAttribute("r", r);
     c.setAttribute("fill", "green");
     c.addEventListener("click", circleClick, true);
     return c;
-}
+};
 
 var drawDot = function(e) {
     if(this==e.target){
@@ -38,24 +42,57 @@ var drawDot = function(e) {
 	svg.appendChild( dot );
     }
     console.log("SVG"+e.target);
-}
+};
 
 var clear = function(e) {
+    window.cancelAnimationFrame( rid );
     while (svg.lastChild){
 	svg.removeChild(svg.lastChild);
     }
+    move = false;
 };
 
 var movedots = function(e){
-
+    
     // do stuff
+    move = !move;
+    
+};
 
-}
+var bounce = function(e) {
+    
+    window.cancelAnimationFrame( rid );
+
+	for (i = 0; i < svg.childNodes.length; i++) {
+	    var circle = svg.childNodes[i];
+
+	    if (parseInt(circle.getAttribute("cx"))+20 >= 500) {
+		circle.setAttribute("vx",-1);
+	    }
+	    if (parseInt(circle.getAttribute("cy"))+20 >= 500) {
+		circle.setAttribute("vy",-1);
+	    }
+	    if (parseInt(circle.getAttribute("cx"))-20 <= 0) {
+		circle.setAttribute("vx",1);
+	    }
+	    if (parseInt(circle.getAttribute("cy"))-20 <= 0) {
+		circle.setAttribute("vy",1);
+	    }	    
+	    
+	    svg.childNodes[i].setAttribute("cx", parseFloat(circle.getAttribute("cx")) + parseInt(circle.getAttribute("vx")));
+	    svg.childNodes[i].setAttribute("cy", parseFloat(circle.getAttribute("cy")) + parseInt(circle.getAttribute("vy")));
+	}
+
+
+    
+    rid = window.requestAnimationFrame(bounce);
+};
 
 var clearBtn = document.getElementById("clear_btn");
 clearBtn.addEventListener("click", clear);
 
 var moveb = document.getElementById("move");
-moveb.addEventListener("click", movedots);
+moveb.addEventListener("click", bounce);
 
 svg.addEventListener("click", drawDot);//, true);
+
